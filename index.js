05b00234faf6dd1cf99b7efa5266199571e5fc38
yoohooitstoo <stumanscott@gmail.com>
2020-06-25 17:04:44 -0400

    added the hw files

---
+++ index.js
@@ -0,0 +1,26 @@
+// array of questions for user
+const fs = require("fs");
+const inquirer =  require('inquirer');
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your user name?",
+      name: "username"
+    },
+
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
